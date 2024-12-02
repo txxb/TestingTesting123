a715afbd48c64bd1e85e9eb79833d5037e6c3b8a
karl <karl.morris@temple.edu>
2024-12-02 08:49:02 -0500

    Updated build

--- app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
+++ app/src/main/java/com/example/testingtesting123/UniqueCollection.kt
@@ -3,21 +3,31 @@ package com.example.testingtesting123
 class UniqueCollection {
     private val collection = ArrayList<Item>()
 
+
+    /** Add an item to the collection */
     fun addItem (item: Item) {
         if (!collection.contains(item))
             collection.add(item)
     }
 
+    /** Return an element from the collection at the specified position */
     fun get (position: Int) = collection[position]
 
-    fun remove (item: Item){
+
+    /** Remove an item from the collection */
+    fun remove (item: Item) {
         !collection.remove(item)
     }
 
+
+    // TODO 3: Complete this function such that the pre-written Unit Test (TDD) passes
+
+    /** Clear all items from hte collection */
     fun clear () {
 
     }
 
+    /** Return the side of the collection */
     fun size() = collection.size
 
 
--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -1,7 +1,5 @@
 package com.example.testingtesting123
 
-import org.junit.Assert.*
-
 import org.junit.Before
 import org.junit.Test
 
@@ -14,22 +12,21 @@ class UniqueCollectionTest {
         collection = UniqueCollection()
     }
 
+
+    // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-        collection.addItem(Item("Item1"))
-        val item = collection.get(0)
-        assert(item.name == "Item1")
+
     }
 
+    // TODO 2: Write a test to ensure that only unique items can be added to the collection
+    // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
-        collection.addItem(Item("Item1"))
-        collection.addItem(Item("item1"))
-        collection.addItem(Item("Item2"))
 
-        assert (collection.size() == 2)
     }
 
+    // Test Driven Development (TDD) test - complete specified function so that this test passes
     @Test
     fun clearAllItems() {
         collection.addItem(Item("item1"))
